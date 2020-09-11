f1a3427b0439ec507aa3d8adb53ea42533f4f502
exxxecutor <alcaponeforum@gmail.com>
2020-09-11 09:29:58 +0300

    num1.java updated

--- src/com/company/num1.java
+++ src/com/company/num1.java
@@ -18,11 +18,39 @@ public class num1 {
         printArr(a);
     }
 
+    public static int getArraySum(int[] a) {
+        int sum = 0;
+        for (int j : a) {
+            sum += j;
+        }
+        return sum;
+    }
+
+    public static int getFactorial(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++)
+            fact *= i;
+        return fact;
+    }
+
     private static void printArr(int[] a) {
         System.out.println(Arrays.toString(a));
     }
 
     public static void main(String[] args) {
         solution(10);
+        num1Test.runTests();
+    }
+}
+
+
+class num1Test {
+    public static void testGetFactorial() {
+        assert num1.getFactorial(3) == 6;
+        assert num1.getFactorial(4) == 24;
+    }
+
+    public static void runTests() {
+        testGetFactorial();
     }
 }
